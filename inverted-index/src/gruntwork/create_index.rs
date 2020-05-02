54054e4166e293099673adbbefa2169a65190797
Sushant Rana <sushantr@hcl.com>
2020-05-02 19:55:43 +0530

    removed unnecessary changes

--- inverted-index/src/gruntwork/create_index.rs
+++ inverted-index/src/gruntwork/create_index.rs
@@ -28,7 +28,6 @@ pub fn create_index(content: String, filepath: &str) {
             let value = token;
             let value_str = value.clone();
             let value_str_dup = value.clone();
-            // insert_update(token.to_lowercase(), filepath);
             let cursor_value = collection.find_one(doc! {"title": value}, None).expect("failed to find_one");
             match cursor_value {
                 Some(document) => {
@@ -50,34 +49,4 @@ pub fn create_index(content: String, filepath: &str) {
             }
         }
      }
-}
-
-pub fn insert_update(value: String, path: &str) {
-    // let value_str = value.clone();
-    // let value_str_dup = value.clone();
-    // let client_options = ClientOptions::parse("mongodb://localhost:27017").expect("failed to get client options");
-    // let client = Client::with_options(client_options).expect("failed to get client with_options");
-    // let db = client.database("inverted_index");
-    // let collection = db.collection("inverted_collection");
-    
-    // let cursor_value = collection.find_one(doc! {"title": value}, None).expect("failed to find_one");
-
-    // match cursor_value {
-    //     Some(document) => {
-    //         let filepath_non_mut = document.get("filepath").and_then(Bson::as_array).expect("failed to get filepath");
-    //         let mut filepath_mut = filepath_non_mut.clone();
-    //         filepath_mut.push(bson::to_bson(&path.to_string()).unwrap());
-    //         println!("inserting {:?}", value_str_dup);
-    //         let result_update = collection.update_one(doc! {"title": value_str}, doc! {"title": value_str_dup, "filepath": filepath_mut}, None);
-    //         match result_update {
-    //             Ok(result) => {
-    //                 println!("{:?}", result);
-    //             }
-    //             Err(e) => println!("{:?}", e)
-    //         }
-    //     }
-    //     None => {
-    //         collection.insert_one(doc! {"title": value_str, "filepath": [path]}, None).unwrap();
-    //     }
-    // }
 }
